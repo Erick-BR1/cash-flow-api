bb69ae076de94a962cb9f8f0aa61737673319072
Erick Rodrigues <erickgta343@gmail.com>
2026-08-09 20:23:35 -0300

    feat: validando despesas no caso de uso

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -7,6 +7,21 @@ public class RegisterExpenseUseCase
 {
     public ResponseRegisteredExpense Execute (RequestRegisterExpense request)
     {
+        Validate(request);
         return new ResponseRegisteredExpense();
+    }
+
+    private void Validate(RequestRegisterExpense request)
+    {
+        var titleIsEmpty = string.IsNullOrWhiteSpace(request.Title);
+        if (titleIsEmpty) throw new ArgumentException("Title is required.");
+
+        if (request.Amount <= 0) throw new ArgumentException("Amount must be greater than zero");
+
+        var result = DateTime.Compare(request.Date, DateTime.UtcNow);
+        if (result > 0) throw new ArgumentException("Expenses cannot be for the future.");
+
+        var paymentTypeIsValid = Enum.IsDefined(typeof(ResponseRegisteredExpense), request.PaymentType);
+        if (paymentTypeIsValid == false) throw new ArgumentException("Payment type invalid.");
     }
 }
