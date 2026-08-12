d8831106c49e0ed1d69e9066cddcd5f88b817f40
Erick-Sansix <erick.rodrigues@sansix.com.br>
2026-08-12 15:42:41 -0300

    feat: teste de sucesso para registrar uma despesa.(pt.1)

---
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -0,0 +1,29 @@
+﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Communication.Requests;
+
+namespace Validators.Tests.Expenses.Register;
+
+public class RegisterExpenseValidatorTests
+{
+    [Fact]
+    public void Success()
+    {
+        //Arrange
+        var validator = new RegisterExpenseValidator();
+        var request = new RequestRegisterExpense
+        {
+            Title = "Test",
+            Description = "Test Expense",
+            Amount = 100,
+            PaymentType = CashFlow.Communication.Enums.PaymentType.CreditCard,
+            Date = DateTime.Now.AddDays(-1)
+        };
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        Assert.True(result.IsValid);
+
+    }
+}
