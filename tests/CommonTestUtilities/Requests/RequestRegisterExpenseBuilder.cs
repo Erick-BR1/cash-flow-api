4a09a4abe19c7f0795dac5519df94e96903b4926
Erick-Sansix <erick.rodrigues@sansix.com.br>
2026-08-12 16:44:28 -0300

    feat: implementando pacote de dados falsos

---
+++ tests/CommonTestUtilities/Requests/RequestRegisterExpenseBuilder.cs
@@ -0,0 +1,18 @@
+﻿using Bogus;
+using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
+
+namespace CommonTestUtilities.Requests;
+
+public class RequestRegisterExpenseBuilder
+{
+    public RequestRegisterExpense Build()
+    {
+        return new Faker<RequestRegisterExpense>()
+            .RuleFor(x=>x.Title, f => f.Commerce.ProductName())
+            .RuleFor(x=>x.Description, f => f.Commerce.ProductDescription())
+            .RuleFor(x=>x.Date, f => f.Date.Past())
+            .RuleFor(x=>x.Amount, f => f.Random.Decimal(min: 0, max: 1000))
+            .RuleFor(x=>x.PaymentType, f => f.PickRandom<PaymentType>());
+    }
+}
