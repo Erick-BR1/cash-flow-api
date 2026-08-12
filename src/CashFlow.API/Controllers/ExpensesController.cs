8c8e13e7683cb49d27498b3ce7333021160b4698
Erick-Sansix <erick.rodrigues@sansix.com.br>
2026-08-12 11:41:59 -0300

    feat: middleware para idiomas

--- src/CashFlow.API/Controllers/ExpensesController.cs
+++ src/CashFlow.API/Controllers/ExpensesController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CashFlow.API.Controllers;
 
-[Route("api/[controller]")]
+[Route("api/expenses")]
 [ApiController]
 public class ExpensesController : ControllerBase
 {
