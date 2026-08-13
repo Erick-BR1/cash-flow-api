a52efec7730e3d383d2b16671e9d7d4285d9eea8
Erick-Sansix <erick.rodrigues@sansix.com.br>
2026-08-13 11:29:32 -0300

    feat: criando teste de erro para título vazio

--- tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -1,4 +1,5 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Exception;
 using CommonTestUtilities.Requests;
 using Shouldly;
 
@@ -19,5 +20,20 @@ public class RegisterExpenseValidatorTests
         //Assert
         result.IsValid.ShouldBeTrue();
 
+    }
+
+    [Fact]
+    public void Error_Title_Empty()
+    {
+        var validator = new RegisterExpenseValidator();
+        var request = RequestRegisterExpenseBuilder.Build();
+        request.Title = string.Empty;
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldHaveSingleItem().ErrorMessage.ShouldBe(ResourceErrorMessages.TITLE_IS_REQUIRED);
     }
 }
