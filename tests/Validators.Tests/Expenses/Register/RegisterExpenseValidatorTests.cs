83100bc974be9d9da126468aece030b4c67cbed9
Erick-Sansix <erick.rodrigues@sansix.com.br>
2026-08-13 14:34:17 -0300

    feat: criando todos os tests para os validators do register

--- tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -1,4 +1,5 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Communication.Enums;
 using CashFlow.Exception;
 using CommonTestUtilities.Requests;
 using Shouldly;
@@ -22,12 +23,15 @@ public class RegisterExpenseValidatorTests
 
     }
 
-    [Fact]
-    public void Error_Title_Empty()
+    [Theory]
+    [InlineData("")]
+    [InlineData("   ")]
+    public void Error_Title_Empty(string title)
     {
+        //Arrange
         var validator = new RegisterExpenseValidator();
         var request = RequestRegisterExpenseBuilder.Build();
-        request.Title = string.Empty;
+        request.Title = title;
 
         //Act
         var result = validator.Validate(request);
@@ -35,5 +39,52 @@ public class RegisterExpenseValidatorTests
         //Assert
         result.IsValid.ShouldBeFalse();
         result.Errors.ShouldHaveSingleItem().ErrorMessage.ShouldBe(ResourceErrorMessages.TITLE_IS_REQUIRED);
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    public void Error_Amount_Invalid(decimal amount)
+    {
+        var validator = new RegisterExpenseValidator();
+        var request = RequestRegisterExpenseBuilder.Build();
+        request.Amount = amount;
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldHaveSingleItem().ErrorMessage.ShouldBe(ResourceErrorMessages.AMOUNT_GREATHER_THAN_ZERO);
+    }
+
+    [Fact]
+    public void Error_Expanses_Cannot_Future()
+    {
+        var validator = new RegisterExpenseValidator();
+        var request = RequestRegisterExpenseBuilder.Build();
+        request.Date = DateTime.UtcNow.AddDays(1);
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldHaveSingleItem().ErrorMessage.ShouldBe(ResourceErrorMessages.EXPANSES_CANNOT_FUTURE);
+    }
+
+    [Fact]
+    public void Error_PaymentType_Invalid()
+    {
+        var validator = new RegisterExpenseValidator();
+        var request = RequestRegisterExpenseBuilder.Build();
+        request.PaymentType = (PaymentType)61;
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldHaveSingleItem().ErrorMessage.ShouldBe(ResourceErrorMessages.PAYMENT_TYPE_INVALID);
     }
 }
