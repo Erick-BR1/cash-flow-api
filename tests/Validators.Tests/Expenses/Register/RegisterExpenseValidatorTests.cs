dc0ed3098ac4b0dc3c714be0a497bdd50f290a8b
Erick-Sansix <erick.rodrigues@sansix.com.br>
2026-08-13 10:47:27 -0300

    feat: removendo pacote FluentAssertions e adicionando Shouldly

--- tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -1,6 +1,6 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Register;
 using CommonTestUtilities.Requests;
-using FluentAssertions;
+using Shouldly;
 
 namespace Validators.Tests.Expenses.Register;
 
@@ -17,7 +17,7 @@ public class RegisterExpenseValidatorTests
         var result = validator.Validate(request);
 
         //Assert
-        result.IsValid.Should().BeTrue();
+        result.IsValid.ShouldBeTrue();
 
     }
 }
