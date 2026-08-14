0a426c5d67d13e0726598e9fbf2819721552e8d4
Erick-Sansix <erick.rodrigues@sansix.com.br>
2026-08-14 16:44:05 -0300

    feat: Criando interface e criando repositorio para despesas

--- src/CashFlow.Infraestructure/DataAccess/CashFlowDbContext.cs
+++ src/CashFlow.Infraestructure/DataAccess/CashFlowDbContext.cs
@@ -3,7 +3,7 @@ using CashFlow.Domain.Entities;
 
 namespace CashFlow.Infraestructure.DataAccess;
 
-public class CashFlowDbContext : DbContext
+internal class CashFlowDbContext : DbContext
 {
     public DbSet<Expense> Expenses { get; set; }
 
