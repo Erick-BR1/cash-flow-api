ec00b68ad9f49ecff35956d10efe2759c2c75cdc
Erick-Sansix <erick.rodrigues@sansix.com.br>
2026-08-17 15:13:20 -0300

    feat: registrando dbcontext com injeção de dependências

--- src/CashFlow.Infraestructure/DataAccess/Repositories/ExpensesRepository.cs
+++ src/CashFlow.Infraestructure/DataAccess/Repositories/ExpensesRepository.cs
@@ -1,15 +1,21 @@
 ﻿using CashFlow.Domain.Entities;
 using CashFlow.Domain.Repositories.Expenses;
+using CashFlow.Infraestructure.DataAccess;
 
-namespace CashFlow.Infraestructure.DataAccess.Repositories;
+namespace CashFlow.Infrastructure.DataAccess.Repositories;
 
 internal class ExpensesRepository : IExpensesRepository
 {
+    private readonly CashFlowDbContext _dbContext;
+    public ExpensesRepository(CashFlowDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
     public void Add(Expense expense)
     {
-        var dbContext = new CashFlowDbContext();
+        _dbContext.Expenses.Add(expense);
 
-        dbContext.Expenses.Add(expense);
-        dbContext.SaveChanges();
+        _dbContext.SaveChanges();
     }
 }
