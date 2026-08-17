e933533151b03349add66cae51af580326e3890d
Erick-Sansix <erick.rodrigues@sansix.com.br>
2026-08-17 15:44:38 -0300

    feat usando appsettings para connection string

--- src/CashFlow.Infraestructure/DataAccess/CashFlowDbContext.cs
+++ src/CashFlow.Infraestructure/DataAccess/CashFlowDbContext.cs
@@ -5,15 +5,7 @@ namespace CashFlow.Infraestructure.DataAccess;
 
 internal class CashFlowDbContext : DbContext
 {
+    public CashFlowDbContext(DbContextOptions options) : base(options) {}
+
     public DbSet<Expense> Expenses { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        var connectionString = "Server=localhost;Database=cashflowdb;Uid=root;Pwd=Roocketseat@6x1";
-        
-        var version = new Version(8, 0, 46);
-        var serverVersion = new MySqlServerVersion(version);
-
-        optionsBuilder.UseMySql(connectionString, serverVersion);
-    }
 }
