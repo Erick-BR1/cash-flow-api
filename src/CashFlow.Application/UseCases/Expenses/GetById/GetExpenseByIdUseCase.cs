a70e83e4bd42c82d935f88e1eedd48c7442d47f1
Erick Rodrigues <erickgta343@gmail.com>
2026-08-25 22:06:16 -0300

    feat: Tratando exception quando ID não existe

--- src/CashFlow.Application/UseCases/Expenses/GetById/GetExpenseByIdUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/GetById/GetExpenseByIdUseCase.cs
@@ -1,7 +1,8 @@
 ﻿using AutoMapper;
-using CashFlow.Communication.Reponses;
 using CashFlow.Communication.Responses;
 using CashFlow.Domain.Repositories.Expenses;
+using CashFlow.Exception;
+using CashFlow.Exception.ExceptionsBase;
 
 namespace CashFlow.Application.UseCases.Expenses.GetById;
 
@@ -19,6 +20,11 @@ public class GetExpenseByIdUseCase : IGetExpenseByIdUseCase
     public async Task<ResponseExpense> Execute(long id)
     {
         var result = await _repository.GetById(id);
+
+        if (result is null)
+        {
+            throw new NotFoundException(ResourceErrorMessages.EXPENSE_NOT_FOUND);
+        }
 
         return _mapper.Map<ResponseExpense>(result);
     }
